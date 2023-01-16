47f133756e149ed4250ccc4c28e6658e93d2e2d3
gaasefod <gaasefod@tutanota.com>
2023-01-16 17:36:44 +0100

    Update toggleterm config

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -1,6 +1,16 @@
 require("toggleterm").setup {
 	size = 70,
-	open_mapping = "<C-t>",
+  open_mapping = "<C-t>",
 	direction = "vertical",
-	autochdir = true
+	autochdir = false,
+  start_in_insert = true,
 }
+
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set("t", "<A-space>", [[<C-\><C-n>]], opts)
+  vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+end
