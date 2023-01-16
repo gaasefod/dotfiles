423e570c091318711cb27da7ba0c251099d0a0d4
gaasefod <gaasefod@tutanota.com>
2023-01-16 17:36:29 +0100

    Add/move autocmds to correct file

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -1,6 +1,18 @@
+-- On file write, source the file and sync the packer plugins
+vim.cmd([[
+  augroup packer_sync
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- Format rust files on save
 vim.cmd([[
   augroup rustfmt
     autocmd!
     autocmd BufWritePre *.rs :RustFmt 
   augroup end
 ]])
+
+-- Make it easier to navigate with a terminal open
+vim.cmd([[autocmd! TermOpen term://* lua set_terminal_keymaps()]])
