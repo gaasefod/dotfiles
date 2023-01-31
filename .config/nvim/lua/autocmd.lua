43d8b997491ce5369c6f3fafde73105a2b6636ba
gaasefod <gaasefod@tutanota.com>
2023-01-31 23:02:30 +0100

    Update autocmds

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -1,18 +1,29 @@
+local autocmd = vim.api.nvim_create_autocmd;
+local augroup = vim.api.nvim_create_augroup
+
 -- On file write, source the file and sync the packer plugins
-vim.cmd([[
-  augroup packer_sync
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
+autocmd({"BufWritePost"}, {
+  pattern = "plugins.lua",
+  command = "source <afile> | PackerSync",
+  group = augroup("packer_sync", {clear = true})
+})
 
 -- Format rust files on save
-vim.cmd([[
-  augroup rustfmt
-    autocmd!
-    autocmd BufWritePre *.rs :RustFmt 
-  augroup end
-]])
+autocmd({"BufWritePre"}, {
+  pattern = "*.rs",
+  command = "RustFmt",
+  group = augroup("rustfmt", {clear = true})
+})
 
 -- Make it easier to navigate with a terminal open
-vim.cmd([[autocmd! TermOpen term://* lua set_terminal_keymaps()]])
+autocmd({"TermOpen"}, {
+  pattern = "term://*",
+  callback = function()
+    local opts = {buffer = 0}
+    vim.keymap.set("t", "<C-space>", [[<C-\><C-n>]], opts)
+    vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+    vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+    vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+    vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+  end
+})
--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -6,12 +6,3 @@ require("toggleterm").setup {
 	autochdir = false,
   start_in_insert = true,
 }
-
-function _G.set_terminal_keymaps()
-  local opts = {buffer = 0}
-  vim.keymap.set("t", "<A-space>", [[<C-\><C-n>]], opts)
-  vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
-end
